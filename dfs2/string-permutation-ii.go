7abb7a57c285f7bda6178389256c5e14c05c1c78
luxcgo <luxcgo@gmail.com>
2022-06-09 22:44:49 +0800

    dfs2: perm. string

---
+++ dfs2/string-permutation-ii.go
@@ -0,0 +1,41 @@
+package dfs2
+
+import "sort"
+
+func StringPermutation(str string) []string {
+	permutations := []string{}
+	if str == "" {
+		return permutations
+	}
+	chars := []rune(str)
+	sort.Slice(chars, func(i, j int) bool {
+		return chars[i] < chars[j]
+	})
+
+	visited := make([]bool, len(chars))
+	stringPermutationDfs(chars, visited, []rune{}, &permutations)
+	return permutations
+}
+
+func stringPermutationDfs(chars []rune, visited []bool, permutation []rune, permutations *[]string) {
+	if len(permutation) == len(chars) {
+		*permutations = append(*permutations, string(permutation))
+	}
+
+	for i, char := range chars {
+		if visited[i] {
+			continue
+		}
+
+		if i > 0 && char == chars[i-1] && !visited[i-1] {
+			continue
+		}
+
+		visited[i] = true
+		permutation = append(permutation, char)
+		stringPermutationDfs(chars, visited, permutation, permutations)
+		permutation = permutation[:len(permutation)-1]
+
+		visited[i] = false
+	}
+}
---
+++ dfs2/string-permutation-ii_test.go
@@ -0,0 +1,11 @@
+package dfs2_test
+
+import (
+	"testing"
+
+	"github.com/go-rythm/n9r/dfs2"
+)
+
+func TestStringPermutation(t *testing.T) {
+	t.Log(dfs2.StringPermutation("abb"))
+}
